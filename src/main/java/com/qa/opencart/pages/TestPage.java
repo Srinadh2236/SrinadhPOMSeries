d3304029a5b3e5f2c0bf0987ed3dd9d7b2155898
Srinadh2236 <sri2reply@gmail.com>
2024-10-01 21:29:31 +0530

    Create TestPage.java

---
+++ src/main/java/com/qa/opencart/pages/TestPage.java
@@ -0,0 +1,16 @@
+package com.qa.opencart.pages;
+
+import org.openqa.selenium.By;
+
+public class TestPage {
+	
+	By loc =By.cssSelector(".Test");
+	
+	public int getTestpage() {
+		System.out.println("click on test page");
+		return 0;
+	}
+	
+	
+
+}
