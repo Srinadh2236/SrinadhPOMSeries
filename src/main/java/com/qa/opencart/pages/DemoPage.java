44a53203c5f9de88748ea9e8b0fbe7bf0d14aca7
Srinadh2236 <sri2reply@gmail.com>
2024-10-01 22:16:08 +0530

    comment added

--- src/main/java/com/qa/opencart/pages/DemoPage.java
+++ src/main/java/com/qa/opencart/pages/DemoPage.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.By;
 public class DemoPage {
 	
 	By loc =By.cssSelector(".demo");
-	
+	//git practice for branch to master 
 	public int getpage() {
 		System.out.println("click on demo page");
 		return 0;
