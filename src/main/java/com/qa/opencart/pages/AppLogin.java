06d83fa63bdbb5fec8fd4259fb2e61bc46b6dfa6
Srinadh2236 <sri2reply@gmail.com>
2024-10-04 22:26:33 +0530

    Update AppLogin.java

--- src/main/java/com/qa/opencart/pages/AppLogin.java
+++ src/main/java/com/qa/opencart/pages/AppLogin.java
@@ -2,10 +2,10 @@ package com.qa.opencart.pages;
 
 public class AppLogin {
 
-	String name="Srinadh";
+	String name="Naveen";
 	
 	public void login() {
-		System.out.println("Srinadh...login");
+		System.out.println("Naveen...login");
 	}
 	
 }
